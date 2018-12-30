af99ba922ab4a926f70255fc4d2a00b161cff65d
zzyyppqq <zzyyppqq@gmail.com>
2018-12-30 08:11:05 +0800

    Initial Commit

---
+++ FirstAppDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FirstAppDemo
+//
+//  Created by zhangyipeng on 2018/12/30.
+//  Copyright © 2018年 zhangyipeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FirstAppDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirstAppDemo
+//
+//  Created by zhangyipeng on 2018/12/30.
+//  Copyright © 2018年 zhangyipeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
